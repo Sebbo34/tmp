f81510d9598da92c9e9ed7e1cdbd61eb33dd9e29
Sebastien BO <seb@MacBook-Air-de-Sebastien.local>
2024-07-20 10:49:26 +0200

    09

--- cpp08/ex02/MutantStack.hpp
+++ cpp08/ex02/MutantStack.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   MutantStack.hpp                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sbo <sbo@student.42.fr>                    +#+  +:+       +#+        */
+/*   By: seb <seb@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/28 23:55:43 by sbo               #+#    #+#             */
-/*   Updated: 2024/06/29 18:46:10 by sbo              ###   ########.fr       */
+/*   Updated: 2024/07/18 16:20:15 by seb              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,49 +17,10 @@
 
 
 template<typename T>
-class MutantStack
+class MutantStack : public stack
 {
-	private :
-		std::stack<T> mStack;
 	public :
-		void	pop(void)
-		{
-			this->mStack.pop();
-		}
-		void	push(T value)
-		{
-			this->mStack.push(value);
-		}
-		T	&top(T value)
-		{
-			return (this->mStack.top(value));
-		}
-		int size(void)
-		{
-			return (this->mStack.size());
-		}
-		bool empty()
-		{
-			return (this->mStack.empty())
-		}
 		
-		class iterator 
-		{
-			private :
-				T *it;
-			public :
-				// Constructor
-				// destructor
-				// = 
-				// copy
-				// overload ++ 
-				// overload +
-				// overload -
-				// overload --
-				// overload *
-				// overload == 
-				// overload !=
-		};
 		
 };
 
